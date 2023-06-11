8f74cbeba8b404520382016a15a0bdf3535395a0
Jabutic4ba <115402883+Jabutic4ba@users.noreply.github.com>
2023-06-11 20:26:41 -0300

    Novo update. Estilo
    Novo design e código. Não estou conseguindo colocar o estilo da pagina em um arquivo css. Ele simplesmente não renderiza quando inicio o servidor. Porém o estilo interno funciona.  Trabalhei mais o design da pagina nesse commit

--- NodeJS/Labirinto/index.js
+++ NodeJS/Labirinto/index.js
@@ -1,10 +1,18 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 http.createServer(function(req, res){
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.write('Bem vindo ao labirinto!');
- res.end('Resolva os desafios e encontre a porta secreta com o tesouro.<br><br> Cuidado em sua aventura, nao sabe o que pode encontrar!<br><br> Sera que e bom o bastante?');
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data){
+        if(err){
+            res.writeHead(404, {'Content-Type':'text/html'});
+            return res.end("404 Not Found");
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080);
-
-
-//preciso saber como acessar paginas diferentes
