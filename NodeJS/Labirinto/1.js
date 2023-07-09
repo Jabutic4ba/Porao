c4dbf0216370a506cd8c537b0607eb96b6eb2fa7
Jabutic4ba <115402883+Jabutic4ba@users.noreply.github.com>
2023-07-08 22:09:30 -0300

    hoje estava inspirado
    A programação fluiu naturalmente com nenhum erro e sem necessitar de muitos testes para atingir as expectativas.

--- NodeJS/Labirinto/1.js
+++ NodeJS/Labirinto/1.js
@@ -1,4 +1,23 @@
 var upperNumbers = document.getElementsByClassName('upperh');
 var lowerNumbers = document.getElementsByClassName('lowerh');
 
-lowerNumbers[0].style.opacity = 1;
+
+function lAction(index){
+	if(isUp(index)){
+		document.getElementsByClassName('lStick')[index].style.display = 'none';
+		document.getElementsByClassName('lStickDown')[index].style.display = 'block';
+		upperNumbers[index].style.opacity = 0.5;
+		lowerNumbers[index].style.opacity = 1;
+	}
+	else{
+		document.getElementsByClassName('lStick')[index].style.display = 'block';
+		document.getElementsByClassName('lStickDown')[index].style.display = 'none';
+		upperNumbers[index].style.opacity = 1;
+		lowerNumbers[index].style.opacity = 0.5;	
+	}
+
+	function isUp(i){
+		return document.getElementsByClassName('lStick')[i].style.display == 'block';
+	}
+}
+
