0618af2a5fd6f3f9c8ad5ae4df2140252d4e4670
Jabutic4ba <115402883+Jabutic4ba@users.noreply.github.com>
2023-09-02 20:47:18 -0300

    Desafio sala 2
    Fazendo o desafio da sala 2 que consiste em encontrar a mensagem escondida em alguma lugar da imagem.

--- NodeJS/Labirinto/2.js
+++ NodeJS/Labirinto/2.js
@@ -1,3 +1,6 @@
+var diffx = 0;
+var diffy = 0;
+
 function magnify(imgID, zoom){
 	var img, glass, w, h, bw;
 	img = document.getElementById(imgID);
@@ -46,12 +49,31 @@ function magnify(imgID, zoom){
     /* Calculate the cursor's x and y coordinates, relative to the image: */
         x = e.pageX - a.left;
         y = e.pageY - a.top;
+
+        diffx = x - 366;
+        diffy = y - 226;
+        var rx = Math.abs(diffx / 48);
+        var ry = Math.abs(diffy / 35);
+        var i = Math.round((rx+ry)/2);
+        document.getElementById('bg').src = 'strangeAnim/strangeanim' + i + '.PNG';
+
+        console.log('x: ' + x + " " + "y: " + y);
+        console.log('x - 366: ' + (x - 366) + ' ' + 'y - 226: ' + (y - 226));
     /* Consider any page scrolling: */
         x = x - window.pageXOffset;
         y = y - window.pageYOffset;
         return {x : x, y : y};
 
+
 	}
 }
 
 magnify("bg", 3);
+
+
+
+/*switch(diffx){
+ case (diffx <= 38 || diffx >= -38) && (diffy <= 25 || diffy >= -25):
+ 	document.getElementById('bg').src = 'strangeAnim/strangeAnim8'
+
+}*/
