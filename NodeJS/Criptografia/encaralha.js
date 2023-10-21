293f70d05ef74a725be8c160a99b95e1d1e8d4fd
Jabutic4ba <115402883+Jabutic4ba@users.noreply.github.com>
2023-10-21 20:42:18 -0300

    Adicionei desencaralha

--- NodeJS/Criptografia/encaralha.js
+++ NodeJS/Criptografia/encaralha.js
@@ -1,34 +1,47 @@
 //Algoritmo de criptografia que embaralha o texto
 
-var key = Buffer.from('Era uma vez uma borboleta');
-var encaralhado = '';
-var arr = [];
+var key = Buffer.from('Calma borboleto');
 
 
-var i = 0;
-//apresenta na forma decimal
-for(x of key.entries()){
-	arr.push(String.fromCharCode(x[1]));
+function encaralha(k){
+ var encaralhado = ['',''];
+ var arr = [];
+ var i = 0;
+ //apresenta na forma decimal
+ for(x of k.entries()){
+ 	arr.push(String.fromCharCode(x[1]));
 	i++;
-}
+ }
 
-for(var i = 0; i < arr.length - 1;i++){
+ var a = arr.length;
+ var desKey = [];
+ for(var i = 0; i < a;i++){
     rand = Math.round(Math.random() * (arr.length - 1));
-	encaralhado += arr[rand];
-	var x = arr.splice(rand, 1);
-	console.log(x);
-	console.log(arr.length);
+	encaralhado[0] += arr[rand];
+	desKey.push(rand);
+	
+	//var x = arr.splice(rand, 1);
+ }
+ encaralhado[1] = desKey;
+ return encaralhado;
 }
 
-console.log(encaralhado);
+function desencaralha(text, chave){
+ var t = Buffer.from(text);
+
+ var desencaralhado = [];
+
+ for(x of t.entries()){
+   console.log(x);
+   desencaralhado[chave[x[0]]] = String.fromCharCode(x[1]);
+   
+ }
+ 
+ return desencaralhado.toString();
+}
+
+console.log(desencaralha('mlCeloaol oaomr', [3,11,0,12,2,14,1,10,11,5,10,1,7,3,8]));
 
 
-
-//A letra 'r' repetiu mais que o necessário
-
-//As letras estão se repetindo :(
-
-//Dá pra remover um elemento do array? <--- Tem jeito :)
-
-
-//Verificar arr.length no for
+//Investigar Buffer.swap16() Buffer.swap32() Buffer.swap64()
+//Numeros da chave repetindo
