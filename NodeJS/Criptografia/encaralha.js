3e693e050f898ed3dbe96a0054e711d3ddf3c9fb
Jabutic4ba <115402883+Jabutic4ba@users.noreply.github.com>
2023-10-28 20:13:22 -0300

    Finalizado

--- NodeJS/Criptografia/encaralha.js
+++ NodeJS/Criptografia/encaralha.js
@@ -1,47 +1,57 @@
 //Algoritmo de criptografia que embaralha o texto
 
-var key = Buffer.from('Calma borboleto');
+var key = Buffer.from('Tava no fluxo e avistei menina no grau');
 
 
 function encaralha(k){
  var encaralhado = ['',''];
  var arr = [];
- var i = 0;
+
  //apresenta na forma decimal
  for(x of k.entries()){
  	arr.push(String.fromCharCode(x[1]));
-	i++;
+
  }
 
- var a = arr.length;
+
  var desKey = [];
- for(var i = 0; i < a;i++){
-    rand = Math.round(Math.random() * (arr.length - 1));
-	encaralhado[0] += arr[rand];
-	desKey.push(rand);
+ for(var i = 0; i < arr.length;i++){
+
+    rand = Math.round(Math.random() * arr.length -1);
+
+	while(desKey.indexOf(rand) != -1){
+		rand = Math.round(Math.random() * arr.length -1);
+		if(arr.length == 0){break;}
+	}
 	
-	//var x = arr.splice(rand, 1);
+    encaralhado[0] += arr[rand];
+	desKey.push(rand);
+
  }
  encaralhado[1] = desKey;
- return encaralhado;
+ return 'Texto: ' + encaralhado[0] + ' || ' + ' Chave: ' + encaralhado[1];
 }
 
 function desencaralha(text, chave){
  var t = Buffer.from(text);
 
- var desencaralhado = [];
+ const d = [];
+ var desentxt = '';
 
  for(x of t.entries()){
-   console.log(x);
-   desencaralhado[chave[x[0]]] = String.fromCharCode(x[1]);
+
+   d[chave[x[0]]] = String.fromCharCode(x[1]);
    
  }
- 
- return desencaralhado.toString();
+ for(var i = 0;i<d.length;i++){
+ 	desentxt += d[i];
+ }
+
+ return desentxt;
 }
 
-console.log(desencaralha('mlCeloaol oaomr', [3,11,0,12,2,14,1,10,11,5,10,1,7,3,8]));
+console.log(desencaralha('ovoeateisnaa eifniuTra  nlg u xn  vmoa', [32,2,12,14,3,20,21,27,19,5,1,29,33,25,18,8,31,22,10,0,35,16,30,7,28,9,34,4,37,13,11,26,15,23,17,24,6,36]));
+//console.log(encaralha(key));
 
 
 //Investigar Buffer.swap16() Buffer.swap32() Buffer.swap64()
-//Numeros da chave repetindo
