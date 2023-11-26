d2eb786a94e868cd88728f3b853b3a05ba2f8a1c
Jabutic4ba <115402883+Jabutic4ba@users.noreply.github.com>
2023-11-26 19:42:02 -0300

    Novo projeto
    Descobri que criar um hardlink não é a mesma coisa que copiar um arquivo.

--- NodeJS/Labirinto/index.js
+++ NodeJS/Labirinto/index.js
@@ -8,7 +8,7 @@ http.createServer(function(req, res){
     fs.readFile(filename, function(err, data){
         if(err){
             res.writeHead(404, {'Content-Type':'text/html'});
-            return res.end("404 Not Found");
+            return res.end("Nao esta aqui :(");
         }
 
         res.writeHead(200, {'Content-Type': 'text/html'});
